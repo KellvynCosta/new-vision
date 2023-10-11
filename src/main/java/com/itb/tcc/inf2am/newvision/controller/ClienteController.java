83be161ddb20d46886f70db588c5f6e53da8e93f
KellvynCosta <kellvyn.thiagocosta@gmail.com>
2023-10-10 21:55:02 -0300

    Atualização do CRUD

---
+++ src/main/java/com/itb/tcc/inf2am/newvision/controller/ClienteController.java
@@ -0,0 +1,73 @@
+package com.itb.tcc.inf2am.newvision.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itb.tcc.inf2am.newvision.model.Cliente;
+import com.itb.tcc.inf2am.newvision.repository.ClienteRepository;
+
+@Controller
+@RequestMapping("/newvision/cliente")
+public class ClienteController {
+	
+	@Autowired // Injeção de depêndencia ( o objeto será criado automaticamente pelo spring)
+	private ClienteRepository clienteRepository;
+	
+	// Carregar o formulário de Cadastro de Usuário
+	
+	@GetMapping("/novo-cliente")
+	public String novoCliente(Cliente cliente, Model model) {
+		
+		model.addAttribute("cliente", cliente);
+		return "cadastro-nvs";
+		
+	}
+	
+	// Inserir Cliente
+	
+	
+	@PostMapping("/add-cliente")
+	public String addCliente(Cliente cliente, Model model) {
+		
+		cliente.setCodStatusCliente(true);
+		
+		Cliente clienteDb = clienteRepository.save(cliente);
+		
+		return "redirect:/newvision/cliente/login";
+	}
+	
+	@GetMapping("/login")
+	public String showFormLogin () {
+
+		return "login-nvs";
+	}
+	
+	@PostMapping("/login")
+	public String autenticarCliente(Cliente cliente, Model model) {
+		Cliente acesso = this.clienteRepository.acessar(cliente.getEmail(), cliente.getSenha());
+		if (acesso != null){
+			return "redirect:/newvision/cliente/home";
+		}
+		return "login-nvs";
+		
+	}
+	
+
+	@GetMapping("/home")
+	public String showHome(Cliente cliente, Model model) {
+		model.addAttribute("cliente", cliente);
+		return "index";
+	}
+	
+
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/com/itb/tcc/inf2am/newvision/NewVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2am.newvision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
